66f8eef5a7aa7eac655ba6d83f147dba92744019
Surhid Amatya <surhidamatya@lftechnology.com>
2017-06-19 13:56:32 +0545

    first commit

---
+++ app/src/main/java/com/surdroid/networkchangedetect/NetworkUtil.java
@@ -0,0 +1,61 @@
+package com.surdroid.networkchangedetect;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkUtil {
+
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+
+    public static String getConnectivityStatusString(Context context) {
+        int conn = NetworkUtil.getConnectivityStatus(context);
+        String status = null;
+        if (conn == NetworkUtil.TYPE_WIFI) {
+            status = "Wifi enabled";
+        } else if (conn == NetworkUtil.TYPE_MOBILE) {
+            status = "Mobile data enabled";
+        } else if (conn == NetworkUtil.TYPE_NOT_CONNECTED) {
+            status = "Not connected to Internet";
+        }
+        return status;
+    }
+
+    public static void getSSIDList(Context context){
+        /*
+        * <uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
+        * Check this permission in manifest
+        * */
+        WifiManager wifiManager = (WifiManager) context
+                .getSystemService(Context.WIFI_SERVICE);
+        List<WifiConfiguration> configuredList = wifiManager.getConfiguredNetworks();
+        List<String> ssidList = new ArrayList<>();
+
+        for(WifiConfiguration config : configuredList) {
+            ssidList.add(config.SSID);
+        }
+    }
+}
